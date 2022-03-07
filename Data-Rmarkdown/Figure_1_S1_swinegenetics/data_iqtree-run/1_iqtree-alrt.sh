0ef075fa116db838bcd37e88e6ca4997d03f3200
TavisAnderson <tavis.anderson@usda.gov>
2022-03-07 14:58:04 -0600

    removing system files

--- Data-Rmarkdown/Figure_1_S1_swinegenetics/data_iqtree-run/1_iqtree-alrt.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-for f in *mauto.fa 
-do 
-nohup iqtree -s $f -nt AUTO -alrt 1000 >& ${f%.fa}.log.txt &
-
-done
-
-
--- Data-Rmarkdown/Figure_1_S1_swinegenetics/data_iqtree-run/2_fixiqtreenames.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-for f in *.treefile
-do python iqtree_namefix.py $f
-done 
-
-
